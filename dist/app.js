6d38b3b427b269761d024e11287f6550120dd6c3
Maddy <mathansivavel95@gmail.com>
2022-06-02 00:20:14 +0530

    3dcube

---
+++ dist/app.js
@@ -0,0 +1,44 @@
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
+
+class App {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+
+    this.camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    this.camera.position.set(0, 0, 4);
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xaaaaaa);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(this.renderer.domElement);
+
+    this.renderer.setAnimationLoop(this.render.bind(this));
+
+	const geometry = new THREE.BoxBufferGeometry();
+	const material = new THREE.MeshStandardMaterial({ color: 0xff0000});
+	
+  this.mesh = new THREE.Mesh(geometry, material);
+  this.scene.add(this.mesh);
+
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {}
+
+  render() {
+	this.mesh.rotateY( 0.01 );
+	this.renderer.render( this.scene, this.camera);
+  }
+}
+
+export { App };
